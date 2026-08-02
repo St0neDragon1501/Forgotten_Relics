be0081d40345154bd2af6f117bb7a4610684f287
kalsibbel <kalsibbel@kals-air.lan>
2026-08-02 14:36:20 -0700

    I added a Item named hope to get a better under standing of the eclips system.

--- forgotten_relics-26.2/src/main/java/net/ok/forgotten_relics/ForgottenRelics.java
+++ forgotten_relics-26.2/src/main/java/net/ok/forgotten_relics/ForgottenRelics.java
@@ -33,36 +33,49 @@ import net.neoforged.neoforge.registries.DeferredRegister;
 
 // The value here should match an entry in the META-INF/neoforge.mods.toml file
 @Mod(ForgottenRelics.MODID)
+
 public class ForgottenRelics {
+	
     // Define mod id in a common place for everything to reference
     public static final String MODID = "forgotten_relics";
+    
     // Directly reference a slf4j logger
     public static final Logger LOGGER = LogUtils.getLogger();
+    
     // Create a Deferred Register to hold Blocks which will all be registered under the "forgotten_relics" namespace
     public static final DeferredRegister.Blocks BLOCKS = DeferredRegister.createBlocks(MODID);
+    
     // Create a Deferred Register to hold Items which will all be registered under the "forgotten_relics" namespace
     public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(MODID);
+    
     // Create a Deferred Register to hold CreativeModeTabs which will all be registered under the "forgotten_relics" namespace
     public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS = DeferredRegister.create(Registries.CREATIVE_MODE_TAB, MODID);
 
     // Creates a new Block with the id "forgotten_relics:example_block", combining the namespace and path
     public static final DeferredBlock<Block> EXAMPLE_BLOCK = BLOCKS.registerSimpleBlock("example_block", p -> p.mapColor(MapColor.STONE));
+    
     // Creates a new BlockItem with the id "forgotten_relics:example_block", combining the namespace and path
     public static final DeferredItem<BlockItem> EXAMPLE_BLOCK_ITEM = ITEMS.registerSimpleBlockItem("example_block", EXAMPLE_BLOCK);
 
     // Creates a new food item with the id "forgotten_relics:example_id", nutrition 1 and saturation 2
     public static final DeferredItem<Item> EXAMPLE_ITEM = ITEMS.registerSimpleItem("example_item", p -> p.food(new FoodProperties.Builder()
-            .alwaysEdible().nutrition(1).saturationModifier(2f).build()));
+    		.alwaysEdible().nutrition(1).saturationModifier(2f).build()));
+    
+    public static final DeferredItem<Item> HOPE = ITEMS.registerSimpleItem("hope");
 
     // Creates a creative tab with the id "forgotten_relics:example_tab" for the example item, that is placed after the combat tab
-    public static final DeferredHolder<CreativeModeTab, CreativeModeTab> EXAMPLE_TAB = CREATIVE_MODE_TABS.register("example_tab", () -> CreativeModeTab.builder()
-            .title(Component.translatable("itemGroup.forgotten_relics")) //The language key for the title of your CreativeModeTab
-            .withTabsBefore(CreativeModeTabs.COMBAT)
-            .icon(() -> EXAMPLE_ITEM.get().getDefaultInstance())
+    public static final DeferredHolder<CreativeModeTab, CreativeModeTab> FORGOTTEN_RELICS_TAB = CREATIVE_MODE_TABS.register("forgotten_relics_tab", () -> CreativeModeTab.builder()
+    		.title(Component.translatable("itemGroup.forgotten_relics")) 
+    		//The language key for the title of your CreativeModeTab.withTabsBefore(CreativeModeTabs.COMBAT)
+            .icon(() -> HOPE.get().getDefaultInstance())
             .displayItems((parameters, output) -> {
-                output.accept(EXAMPLE_ITEM.get());// Add the example item to the tab. For your own tabs, this method is preferred over the event
+            	// Add item to tab
+                output.accept(HOPE.get());
+                output.accept(EXAMPLE_ITEM.get());
             }).build());
 
+    
+    
     // The constructor for the mod class is the first code that is run when your mod is loaded.
     // FML will recognize some parameter types like IEventBus or ModContainer and pass them in automatically.
     public ForgottenRelics(IEventBus modEventBus, ModContainer modContainer) {
@@ -71,8 +84,11 @@ public class ForgottenRelics {
 
         // Register the Deferred Register to the mod event bus so blocks get registered
         BLOCKS.register(modEventBus);
+        
         // Register the Deferred Register to the mod event bus so items get registered
         ITEMS.register(modEventBus);
+        
+        
         // Register the Deferred Register to the mod event bus so tabs get registered
         CREATIVE_MODE_TABS.register(modEventBus);
 
@@ -88,6 +104,8 @@ public class ForgottenRelics {
         modContainer.registerConfig(ModConfig.Type.COMMON, Config.SPEC);
     }
 
+    
+    
     private void commonSetup(FMLCommonSetupEvent event) {
         // Some common setup code
         LOGGER.info("HELLO FROM COMMON SETUP");
@@ -107,6 +125,8 @@ public class ForgottenRelics {
             event.accept(EXAMPLE_BLOCK_ITEM);
         }
     }
+    
+    
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
     @SubscribeEvent
