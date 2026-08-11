ee5c66aa057c13ddba97d4b6fe4ca5ca2cf96d83
Kal Sibbel <110316595+SuperLak@users.noreply.github.com>
2026-08-11 03:41:09 -0500

    Added watch's rewind ability w/o sound nor hunger limitation.

--- forgotten_relics-26.2/src/main/java/net/ok/forgotten_relics/item/ForgottenWatch.java
+++ forgotten_relics-26.2/src/main/java/net/ok/forgotten_relics/item/ForgottenWatch.java
@@ -1,26 +1,19 @@
 package net.ok.forgotten_relics.item;
 
-import net.minecraft.client.Minecraft;
-import net.minecraft.network.chat.Component;
+import java.util.Set;
+
 import net.minecraft.server.level.ServerLevel;
-import net.minecraft.server.level.ServerPlayer;
-import net.minecraft.sounds.SoundSource;
+import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
-import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.entity.PositionMoveRotation;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
-import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.TooltipFlag;
-import net.minecraft.world.item.component.TooltipDisplay;
-import net.minecraft.world.item.context.UseOnContext;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.Blocks;
-
-import net.minecraft.core.particles.ParticleTypes; 
 
 
 public class ForgottenWatch extends Item {
+	
+	private static PositionMoveRotation oldPos = null;
 	
 
     public ForgottenWatch(Properties properties) {
@@ -29,14 +22,27 @@ public class ForgottenWatch extends Item {
     
 
     @Override
-    public InteractionResult useOn(UseOnContext context) {
-        Level level = context.getLevel(); 
-        
+    public InteractionResult use(Level level, Player player, InteractionHand hand) {
         if(!level.isClientSide() ) {
-        ((ServerLevel) level).sendParticles(ParticleTypes.HEART,
-                context.getClickedPos().getX() + 0, context.getClickedPos().getY() + 0,
-                context.getClickedPos().getZ() + 0, 0, 0, 0, 0, 0);
+        	if (oldPos == null) {
+            	oldPos = new PositionMoveRotation(player.position(), player.getDeltaMovement(), player.getYRot(), player.getXRot());
+            	
+        	} else {
+        		player.teleportTo(
+                        (ServerLevel) level,
+                        oldPos.position().x,
+                        oldPos.position().y,
+                        oldPos.position().z,
+                        Set.of(),
+                        oldPos.yRot(),
+                        oldPos.xRot(),
+                        true);
+            	
+        		oldPos = null;
+        		
+        	}
         }
+        	
         return InteractionResult.SUCCESS;
     }
 }
