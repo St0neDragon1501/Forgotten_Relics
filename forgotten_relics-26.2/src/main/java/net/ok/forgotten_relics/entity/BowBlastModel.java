fc8e757ad19ce92d76ae51c2ce3d5bacfdaa4366
kalsibbel <kalsibbel@kals-air.lan>
2026-08-14 15:40:11 -0700

    added Final texture

--- forgotten_relics-26.2/src/main/java/net/ok/forgotten_relics/entity/BowBlastModel.java
+++ forgotten_relics-26.2/src/main/java/net/ok/forgotten_relics/entity/BowBlastModel.java
@@ -2,23 +2,18 @@ package net.ok.forgotten_relics.entity;
 
 import net.minecraft.client.model.EntityModel;
 import net.minecraft.client.model.geom.ModelPart;
-import net.minecraft.client.model.geom.PartPose;
 import net.minecraft.client.model.geom.builders.CubeDeformation;
 import net.minecraft.client.model.geom.builders.CubeListBuilder;
 import net.minecraft.client.model.geom.builders.LayerDefinition;
 import net.minecraft.client.model.geom.builders.MeshDefinition;
 import net.minecraft.client.model.geom.builders.PartDefinition;
-import net.minecraft.client.renderer.rendertype.RenderTypes;
 
 public class BowBlastModel extends EntityModel<BowBlastRenderState> {
 
-    private final ModelPart bone;
     private final ModelPart bb_main;
 
     public BowBlastModel(ModelPart root) {
-        super(root, RenderTypes::entityCutoutCull);
-
-        this.bone = root.getChild("bone");
+        super(root);
         this.bb_main = root.getChild("bb_main");
     }
 
@@ -26,112 +21,15 @@ public class BowBlastModel extends EntityModel<BowBlastRenderState> {
         MeshDefinition meshdefinition = new MeshDefinition();
         PartDefinition partdefinition = meshdefinition.getRoot();
 
-        // BONE
-        PartDefinition bone = partdefinition.addOrReplaceChild(
-                "bone",
-                CubeListBuilder.create()
-                        .texOffs(0, 26)
-                        .addBox(
-                                -5.0F, 0.0F, -1.0F,
-                                6.0F, 0.0F, 8.0F,
-                                new CubeDeformation(0.0F)
-                        ),
-                PartPose.offset(2.0F, 17.0F, 11.0F)
-        );
+        PartDefinition bb_main = partdefinition.addOrReplaceChild("bb_main",CubeListBuilder.create(),net.minecraft.client.model.geom.PartPose.offset(0.0F, 12.0F, 0.0F));
+       
+        
+        
+        bb_main.addOrReplaceChild("cube_r1",CubeListBuilder.create().texOffs(0, 0).addBox(-9.0F, 1.0F, -14.0F, 18.0F, 0.0F, 32.0F,new CubeDeformation(0.0F)),net.minecraft.client.model.geom.PartPose.offsetAndRotation(0.0F, -13.0F, -1.0F, -1.5708F, 0.0F, 1.5708F));
 
-        bone.addOrReplaceChild(
-                "cube_r1",
-                CubeListBuilder.create()
-                        .texOffs(28, 26)
-                        .addBox(
-                                -5.0F, 0.0F, -1.0F,
-                                6.0F, 0.0F, 8.0F,
-                                new CubeDeformation(0.0F)
-                        ),
-                PartPose.offsetAndRotation(
-                        -2.0F, -2.0F, 0.0F,
-                        0.0F, 0.0F, -1.5708F
-                )
-        );
+        bb_main.addOrReplaceChild("cube_r2",CubeListBuilder.create().texOffs(0, 0).addBox(-8.0F, 0.0F, -15.0F, 18.0F, 0.0F, 32.0F,new CubeDeformation(0.0F)),net.minecraft.client.model.geom.PartPose.offsetAndRotation(0.0F, -13.0F, -1.0F, 0.0F, 1.5708F, -3.1416F));
 
-        // MAIN BODY
-        PartDefinition bb_main = partdefinition.addOrReplaceChild(
-                "bb_main",
-                CubeListBuilder.create()
-                        .texOffs(1, 35)
-                        .addBox(
-                                -0.5F, -7.5F, -14.0F,
-                                1.0F, 1.0F, 6.0F,
-                                new CubeDeformation(0.0F)
-                        )
-                        .texOffs(16, 41)
-                        .addBox(
-                                -2.0F, -9.0F, -10.0F,
-                                4.0F, 4.0F, 2.0F,
-                                new CubeDeformation(0.0F)
-                        )
-                        .texOffs(40, 41)
-                        .addBox(
-                                -1.0F, -10.0F, -10.0F,
-                                2.0F, 6.0F, 2.0F,
-                                new CubeDeformation(0.0F)
-                        )
-                        .texOffs(0, 0)
-                        .addBox(
-                                -1.0F, -8.0F, -8.0F,
-                                2.0F, 2.0F, 24.0F,
-                                new CubeDeformation(0.0F)
-                        )
-                        .texOffs(28, 41)
-                        .addBox(
-                                -1.5F, -8.5F, -11.0F,
-                                3.0F, 3.0F, 3.0F,
-                                new CubeDeformation(0.0F)
-                        )
-                        .texOffs(17, 35)
-                        .addBox(
-                                -1.0F, -7.5F, -13.0F,
-                                2.0F, 1.0F, 5.0F,
-                                new CubeDeformation(0.0F)
-                        ),
-                PartPose.offset(0.0F, 24.0F, 0.0F)
-        );
-
-        bb_main.addOrReplaceChild(
-                "cube_r2",
-                CubeListBuilder.create()
-                        .texOffs(33, 35)
-                        .addBox(
-                                0.0F, -1.0F, -4.0F,
-                                2.0F, 1.0F, 5.0F,
-                                new CubeDeformation(0.0F)
-                        ),
-                PartPose.offsetAndRotation(
-                        0.5F, -6.0F, -9.0F,
-                        0.0F, 0.0F, -1.5708F
-                )
-        );
-
-        bb_main.addOrReplaceChild(
-                "cube_r3",
-                CubeListBuilder.create()
-                        .texOffs(0, 42)
-                        .addBox(
-                                -1.0F, -2.0F, -1.0F,
-                                2.0F, 6.0F, 2.0F,
-                                new CubeDeformation(0.0F)
-                        ),
-                PartPose.offsetAndRotation(
-                        -1.0F, -7.0F, -9.0F,
-                        0.0F, 0.0F, -1.5708F
-                )
-        );
-
-        return LayerDefinition.create(
-                meshdefinition,
-                64,
-                64
-        );
+        return LayerDefinition.create(meshdefinition, 128, 128);
     }
 
     @Override
